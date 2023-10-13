9c4bbac2aa5855869bce5834e8893eaf78c044c9
PaxDaciae <alexandruiulian.micu@gmail.com>
2023-10-13 14:03:56 +0300

    Several Changes

--- scripts.js
+++ scripts.js
@@ -2,30 +2,29 @@
 
 const changingImagesByTheme = document.getElementsByClassName("color-change-picture");
 
-console.log(Object.values(changingImagesByTheme))
-const notWelcomeText = document.querySelectorAll(":not([id^='welcome-screen-resize'])");
-const welcomeText = document.getElementById('welcome-screen-resize');
-
-// console.log(Object.values(notWelcomeText))
-// notWelcomeText.foreach(element => {
-//     console.log(element)
-//     // element.style.setProperty('display', 'none')
-// })
-
 documentBackground = document.getElementById("background-gradient").classList
 backgroundClasses = Object.values(documentBackground);
 
+let root = document.querySelector(':root')
+let techDemoExpansive = Object.values(document.getElementsByClassName("tech-demo-expansive"))
 function setLightTheme(){
     documentBackground.add("magenta-teal-gradient-background")
     documentBackground.remove("black-blue-gradient-background")
-    document.querySelector(':root').style.setProperty('--text-color','black');
-    document.querySelector(':root').style.setProperty('--text-shadow-color','white');
-    document.querySelector(':root').style.setProperty('--card-background-color','rgba(230, 230, 230, 1)');
+    
+    root.style.setProperty('--text-color','black');
+    root.style.setProperty('--text-shadow-color','white');
+    root.style.setProperty('--card-background-color','rgba(230, 230, 230, 1)');
+
+    techDemoExpansive.forEach(element => {
+        element.style.setProperty('background-color',' rgba(255,255,255, 0.4)')
+    })
+
     document.getElementById("background-color").style.setProperty('background-color', 'white');
 
     document.getElementById("theme-vehicle").style.transform = "translateX(-71.5px)";
 
     document.getElementById("theme-sun-icon").classList.remove('hidden')
+
     document.getElementById("theme-moon-icon").classList.add('hidden')
     
     Object.values(changingImagesByTheme).forEach(image => {
@@ -55,14 +54,16 @@ function setDarkTheme(){
     documentBackground.add("black-blue-gradient-background")
     documentBackground.remove("magenta-teal-gradient-background")
 
-    document.querySelector(':root').style.setProperty('--text-color', 'white');
-    document.querySelector(':root').style.setProperty('--text-shadow-color', 'black');
-    document.querySelector(':root').style.setProperty('--card-background-color', 'white');
+    root.style.setProperty('--text-color', 'white');
+    root.style.setProperty('--text-shadow-color', 'black');
+    root.style.setProperty('--card-background-color', 'white');
+
+    techDemoExpansive.forEach(element => {
+        element.style.setProperty('background-color',' rgba(0,0,0, 0.4)')
+    })
+
     document.getElementById("background-color").style.setProperty('background-color', 'rgba(0,0,0,1)')
-    // document.querySelector(':root').style.setProperty('--background-gradient', 'linear-gradient(135deg, rgba(22,0,148,1) 0%, rgba(0,0,0,1) 100%)');
-
-    // console.log(document.querySelector(':root').style)
-
+   
     document.getElementById("theme-vehicle").style.removeProperty("transform")
 
     document.getElementById("theme-sun-icon").classList.add('hidden')
@@ -138,28 +139,30 @@ document.getElementById("parallax-wrapper").addEventListener("resize", setScroll
 
 function setScrollPercent() {
     const pageElement = document.getElementById("parallax-wrapper")
-    console.log("SCRLTOP", pageElement.scrollTop, "CHEIT", pageElement.clientHeight)
+    // console.log("SCRLTOP", pageElement.scrollTop, "CHEIT", pageElement.clientHeight)
     const percentageOfScreenScrolled = 1 - (pageElement.scrollTop / pageElement.clientHeight)
     document.getElementById('background-gradient').style.setProperty("--scrollPercent", Math.max(percentageOfScreenScrolled, 0))
-    console.log("OPACITY", Math.max(percentageOfScreenScrolled, 0))
-}
-
-function moveTo(target){
-
+    // console.log("OPACITY", Math.max(percentageOfScreenScrolled, 0))
 }
 
 function scrollWindowTo(divID){
     const target = document.getElementById(divID);
     const body = document.getElementById('parallax-wrapper')
     if(divID == 'welcome-section' ){
-        console.log("yes_Maam")
         body.scrollTo(0, -1000)
     } else {
-        body.scrollTo(0, (target.getBoundingClientRect().y - 50))
+        body.scrollTo(0, (target.getBoundingClientRect().y - 10))
 
     }
 }
 
+
+function expand(number){
+    targets=Object.values(document.getElementsByClassName(`expansive${number}`));
+    targets.forEach(target=>
+        target.classList.toggle('expanded'));
+    document.getElementById(`wrapper${number}`).classList.remove('wrapper-hover')
+}
 
 getStoredTheme()
 setScrollPercent()
